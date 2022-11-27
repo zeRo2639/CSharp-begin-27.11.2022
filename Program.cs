673b8e419bd3818092ee98e829b8285cb7269da4
Rezo <dastan2639@gmail.com>
2022-11-27 15:07:43 +0300

    Решили задачу по выводу квадратного числа

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("start");
+
+int number = 11; // определили целое число
+int result = number * number; // вычисление квадрата числа
+Console.WriteLine(result); // вывод результата
+
+Console.WriteLine("end"); // окончание 
