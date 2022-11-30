a322bd9728591327ec7d6485c78837cc7fc6b887
Rezo <dastan2639@gmail.com>
2022-11-30 22:53:55 +0300

    написал программу, которая принимает на вход три числа и выдает максимальное значение из этих чисел

--- HomeWork/Exersise_02/Program.cs
+++ HomeWork/Exersise_02/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("start");
+int a = 3, b = 5, c = 7;
+int max = a;
+if (b > max)
+{
+  max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
+Console.WriteLine("end");
