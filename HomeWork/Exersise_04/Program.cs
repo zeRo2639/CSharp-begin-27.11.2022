f81252e298872c02fb8569322deef41201bcc9af
Rezo <dastan2639@gmail.com>
2022-12-03 23:19:32 +0300

    Решили задачу по выводу четных чисел от 1 до N

---
+++ HomeWork/Exersise_04/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+       int res = 1;
+       while (res < N)
+       {
+        Console.WriteLine(res+1);
+       if (res % 2 == 0)
+        Console.WriteLine(res);
+        res = res + 2;
+       }
