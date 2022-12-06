2bca7554d838e73617d758199e947dba0763dd73
Rezo <dastan2639@gmail.com>
2022-12-06 21:03:39 +0300

    Создали папку Seminar03

---
+++ Seminar03/Program.cs
@@ -0,0 +1,16 @@
+Console.WriteLine("start");
+int [] CreateArray(int size);
+{
+    return new int[size];
+}
+{
+Fill("int[] array, int index, int sum");
+}
+    ConsoleWriteLine("index = 0, sum = 0");
+while (index <= size - 1)
+{
+    if (index[array] % 2 == 0)
+    ConsoleWriteLine("sum = sum + array[index]");
+}
+index = index + 1;
+ConsoleWriteLine("sum");
