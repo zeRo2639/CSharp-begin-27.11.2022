2785ec32566d7f17cd41567f8abb8a204d5367de
Rezo <dastan2639@gmail.com>
2022-12-11 00:12:05 +0300

    написали программу, которая выводит таблицу кубов

---
+++ HomeWork3/Exersise302/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве. 
+// A (0,0); B (1,1), -> ~1.4
+// A (2,4); B (0,7) -> ~3.6
+Console.WriteLine("Start");
+int x1 = 2;
+int y1 = 4;
+int x2 = 0;
+int y2 = 7;
+double distance = 0;
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между двумя точками равно {distance}");
---
+++ HomeWork3/Exersise303/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Start");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+void Cube(int[] cube)
+{
+  int counter = 1;
+  int length = cube.Length;
+  while (counter <  length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+void PrintArray(int[] coll)
+{
+  int count = coll.Length;
+  int index = 1;
+  while(index < count)
+  {
+    Console.WriteLine(coll[index]+ " ");
+    index++;
+  }
+} 
+int[] array = new int[cube+1];
+Cube(array);
+PrintArray(array);
+
+
